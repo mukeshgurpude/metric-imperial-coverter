61d96fa36a495ded8b1e56dc9516efb4780f0eec
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-07-07 17:21:38 +0530

    finish unit tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,5 +1,6 @@
 const mappings = {km: 'mi', mi: 'km', gal: 'L', L: 'gal', lbs: 'kg', kg: 'lbs'}
 const changer = {km: 1/1.60934, mi: 1.60934, lbs: 0.453592, kg: 1/0.453592, gal: 3.78541, L: 1/3.78541}
+const spells = {km: 'kilometers', mi: 'miles', lbs: 'pounds', gal: 'gallons', kg: 'kilograms', L: 'liters'}
 
 function getMid(input){
   const char = input.match(/[a-z]/);
@@ -15,36 +16,27 @@ function ConvertHandler() {
   };
 
   this.getUnit = function(input) {
-    let result;
-
-    return result;
+    const unit = input.slice(getMid(input))
+    if(! unit in mappings) throw new Error('invalid unit');
+    return unit;
   };
 
   this.getReturnUnit = function(initUnit) {
-    let result;
-
-    return result;
+    return mappings[initUnit]
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-
-    return result;
+    return spells[unit];
   };
 
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-
-    return result;
+    return Math.round(initNum*changer[initUnit]*100000)/100000;
   };
 
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
+    const decimals = String(returnNum).match(/(?<=\.)\d*/)[0].length
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum}${'0'.repeat(5-decimals)} ${this.spellOutUnit(returnUnit)}`;
   };
-
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -30,4 +30,50 @@ suite('Unit Tests', function(){
       assert.equal(convertHandler.getNum('km'), 1)
     })
   })
+  suite('Check return units', function(){
+    test('gallon and Liter', function(){
+      assert.equal(convertHandler.getReturnUnit('gal'), 'L')
+      assert.equal(convertHandler.getReturnUnit('L'), 'gal')
+    })
+    test('mile and Kilometers', function(){
+      assert.equal(convertHandler.getReturnUnit('mi'), 'km')
+      assert.equal(convertHandler.getReturnUnit('km'), 'mi')
+    })
+    test('Kilogram and Pound', function(){
+      assert.equal(convertHandler.getReturnUnit('kg'), 'lbs')
+      assert.equal(convertHandler.getReturnUnit('lbs'), 'kg')
+    })
+  })
+  test('Spell out units', function(){
+    assert.equal(convertHandler.spellOutUnit('km'), 'kilometers')
+    assert.equal(convertHandler.spellOutUnit('mi'), 'miles')
+    assert.equal(convertHandler.spellOutUnit('kg'), 'kilograms')
+    assert.equal(convertHandler.spellOutUnit('lbs'), 'pounds')
+    assert.equal(convertHandler.spellOutUnit('gal'), 'gallons')
+    assert.equal(convertHandler.spellOutUnit('L'), 'liters')
+  })
+  test('Check init units', function(){
+    assert.equal(convertHandler.getUnit('3.1mi'), 'mi')
+  })
+  suite('Check full example outputs', function(){
+    function check_all(input, expectedNum, expectedInitUnit, expectedReturnNum, expectedReturnUnit, returnString){
+      const num = convertHandler.getNum(input)
+      const initUnit = convertHandler.getUnit(input)
+      const returnNum = convertHandler.convert(num, initUnit)
+      const returnUnit = convertHandler.getReturnUnit(initUnit)
+
+      assert.strictEqual(num, expectedNum)
+      assert.strictEqual(initUnit, expectedInitUnit)
+      assert.strictEqual(returnNum, expectedReturnNum)
+      assert.strictEqual(returnUnit, expectedReturnUnit)
+      assert.strictEqual(convertHandler.getString(num, initUnit, returnNum, returnUnit), returnString)
+    }
+    test('Check example result', function(){
+      check_all('3.1mi', 3.1, 'mi', 4.98895, 'km', '3.1 miles converts to 4.98895 kilometers')
+    })
+    test('Check custom inputs', function(){
+      check_all('kg', 1, 'kg', 2.20462, 'lbs', '1 kilograms converts to 2.20462 pounds')
+      check_all('2.3L', 2.3, 'L', 0.6076, 'gal', '2.3 liters converts to 0.60760 gallons')
+    })
+  })
 });
